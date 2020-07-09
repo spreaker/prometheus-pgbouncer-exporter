f869050818cb3b891e64aa9f088b5034891a0b8c
Vineet <vineet@zumper.com>
2020-07-09 10:26:28 -0700

    Review 1

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,11 +13,15 @@ CURR_DIR=os.path.dirname(os.path.realpath(__file__))
 
 class TestCLI(unittest.TestCase):
 
-    def testReadConfigFile(self):
+    @mock.patch('prometheus_pgbouncer_exporter.cli.logging')
+    def testReadConfigFile(self, mock_logging):
+        mock_logger = mock.MagicMock()
+        mock_logging.getLogger.return_value = mock_logger
 
         # should read and validate this config file without any issues
         config = cli.read_config_file(CURR_DIR + '/fixtures/config-with-one-pgbouncer.yml')
 
+        self.assertEqual(mock_logger.info.call_count, 1)
         self.assertEqual(config.getExporterHost(), "0.0.0.0")
         self.assertEqual(config.getExporterPort(), 1234)
         self.assertEqual(len(config.getPgbouncers()), 1)
@@ -34,27 +38,29 @@ class TestCLI(unittest.TestCase):
             cli.read_config_file(CURR_DIR + '/path/to/none.yml')
 
         self.assertEqual('Cannot read config file', str(err.exception))
+        self.assertEqual(mock_logger.fatal.call_count, 1)
 
         # Should raise exception saying 'Invalid config file'
         with self.assertRaises(Exception) as err:
             cli.read_config_file(CURR_DIR + '/fixtures/config-empty.yml')
 
         self.assertEqual('Invalid config file', str(err.exception))
+        self.assertEqual(mock_logger.fatal.call_count, 2)
 
     @mock.patch('prometheus_pgbouncer_exporter.cli.os._exit')
     @mock.patch('prometheus_pgbouncer_exporter.collector.PgbouncersMetricsCollector.update')
     @mock.patch('prometheus_pgbouncer_exporter.collector.PgbouncersMetricsCollector.collect')
     @mock.patch('prometheus_pgbouncer_exporter.cli.argparse.ArgumentParser')
-    def testSignalHandling(self, _argparser, _collect, _update, _exit):
+    def testSignalHandling(self, mock_argparser, mock_collect, mock_update, mock_exit):
         pid = os.getpid()
         hup_count = 0
 
         def send_signal():
             nonlocal hup_count
-            # Keep sending HUP until PgbouncersMetricsCollector.update to be called at least twice.
+            # Keep sending HUP until PgbouncersMetricsCollector.update is called at least twice.
             # The first call is made when PgbouncersMetricsCollector class is initialized
             # Any subsequent call happens as a result of config reload from HUP
-            while _update.call_count < 2:
+            while mock_update.call_count < 2:
                 os.kill(pid, signal.SIGHUP)
                 time.sleep(0.5)
                 hup_count += 1
@@ -66,32 +72,26 @@ class TestCLI(unittest.TestCase):
 
         signal.signal(signal.SIGHUP, _on_sighup)
 
-        # mock argparse
-        _args = mock.MagicMock()
-        _args.config = CURR_DIR + '/fixtures/config-with-one-pgbouncer.yml'
-        _args.log_file = 'stdout'
-        _args.log_level = 'INFO'
+        args = mock.MagicMock()
+        args.config = CURR_DIR + '/fixtures/config-with-one-pgbouncer.yml'
+        args.log_file = 'stdout'
+        args.log_level = 'CRITICAL'
 
-        _parser = mock.MagicMock()
-        _parser.parse_args.return_value = _args
-        _argparser.return_value = _parser
+        parser = mock.MagicMock()
+        parser.parse_args.return_value = args
+        mock_argparser.return_value = parser
 
         thread = threading.Thread(target=send_signal)
         thread.daemon = True
         thread.start()
 
-        self.assertEqual(_update.call_count, 0)
-
-        # start main service
+        self.assertEqual(mock_update.call_count, 0)
         cli.main()
-
-        self.assertEqual(_update.call_count, 2)
+        self.assertEqual(mock_update.call_count, 2)
+        mock_exit.assert_called_once_with(0)
 
         # test if hup handler aggregates hup counts and 'update' is not called on every HUP
-        self.assertGreater(hup_count, _update.call_count)
-
-        # check if exit is gracefully called after sigterm
-        _exit.assert_called_once_with(0)
+        self.assertGreater(hup_count, mock_update.call_count)
 
 
 if __name__ == '__main__':
