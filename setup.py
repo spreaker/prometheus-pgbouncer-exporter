b963c0aa2d90741ec37004d9a9fb477346a89e2f
pashamoss <35258062+PavelSorokin@users.noreply.github.com>
2025-03-21 09:16:20 +0000

    add psycopg2==2.9.10

--- setup.py
+++ setup.py
@@ -11,18 +11,18 @@ with open('README.md') as file:
 setup(
   name                          = 'prometheus-pgbouncer-exporter',
   packages                      = ['prometheus_pgbouncer_exporter'],
-  version                       = '2.1.2',
+  version                       = '2.1.3',
   description                   = 'Prometheus exporter for PgBouncer',
   long_description              = long_description,
   long_description_content_type = "text/markdown",
   author                        = 'Spreaker Developers',
   author_email                  = 'dev@spreaker.com',
   url                           = 'https://github.com/spreaker/prometheus-pgbouncer-exporter',
-  download_url                  = 'https://github.com/spreaker/prometheus-pgbouncer-exporter/archive/2.1.1.tar.gz',
+  download_url                  = 'https://github.com/spreaker/prometheus-pgbouncer-exporter/archive/2.1.3.tar.gz',
   keywords                      = ['prometheus', 'pgbouncer'],
   classifiers                   = [],
   python_requires               = ' >= 3',
-  install_requires              = ['psycopg2 == 2.8.5', 'prometheus_client==0.8.0', 'python-json-logger==0.1.11', 'PyYAML>=5.4.1'],
+  install_requires              = ['psycopg2 == 2.9.10', 'prometheus_client==0.8.0', 'python-json-logger==0.1.11', 'PyYAML>=5.4.1'],
   entry_points                  = {
     'console_scripts': [
         'pgbouncer-exporter=prometheus_pgbouncer_exporter.cli:main',
